4b186352c71a5fe8a0e186a6572e794abeebc0fa
purwandas <purwandas@gmail.com>
2016-10-05 07:24:26 +0700

    commit1

---
+++ html2pdf/examples/exemple05.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * HTML2PDF Librairy - example
+ *
+ * HTML => PDF convertor
+ * distributed under the LGPL License
+ *
+ * @author      Laurent MINGUET <webmaster@html2pdf.fr>
+ *
+ * isset($_GET['vuehtml']) is not mandatory
+ * it allow to display the result in the HTML format
+ */
+
+    // get the HTML
+    ob_start();
+    
+include(dirname(__FILE__).'/res/bastwilayah.php');
+    
+$content = ob_get_clean();
+
+    // convert to PDF
+   
+ require_once(dirname(__FILE__).'/../html2pdf.class.php');
+    
+try
+    
+{
+        
+$html2pdf = new HTML2PDF('L', 'legal', 'fr');
+        
+$html2pdf->pdf->SetDisplayMode('fullpage');
+        
+$html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+        
+$html2pdf->Output('exemple05.pdf');
+   
+}
+    
+catch(HTML2PDF_exception $e) 
+{
+        
+echo $e;
+        
+exit;
+    
+}
---
+++ html2pdf/examples/res/sample.php
@@ -0,0 +1,79 @@
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Soal 01 - Table</title>
+<style type="text/css">
+<!--
+.style1 {color: #FFFFFF}
+.style2 {color: #000000; }
+.style3 {font-family: Arial, Helvetica, sans-serif}
+-->
+</style>
+</head>
+
+<body>
+<h1 align="center" class="style3">Histori Nilai Semester Genap 2008/2009</h1>
+<table width="80%" border="8" align="center" cellpadding="5" bgcolor="#0000FF">
+  <tr>
+    <th rowspan="2" >Matakuliah</th>
+    <th colspan="3" >Nilai</th>
+    <th rowspan="2" >Nilai Akhir</th>
+    <th rowspan="2" >Grade</th>
+  </tr>
+  <tr>
+    <th>Tugas</th>
+    <th>UTS</th>
+    <th>UAS</th>
+  </tr>
+  <tr>
+    <td><span class="style1">Algoritma dan Struktur Data 1 </span></td>
+    <td><div align="center" class="style1">80</div></td>
+    <td><div align="center" class="style1">90</div></td>
+    <td><div align="center" class="style1">85</div></td>
+    <td><div align="center" class="style1">86,5</div></td>
+    <td bgcolor="#FFFF00"><div align="center" class="style2">A</div></td>
+  </tr>
+  <tr>
+    <td><span class="style1">Pemrograman Web </span></td>
+    <td><div align="center" class="style1">90</div></td>
+    <td><div align="center" class="style1">80</div></td>
+    <td><div align="center" class="style1">70</div></td>
+    <td><div align="center" class="style1">76,0</div></td>
+    <td bgcolor="#FFFF00"><div align="center" class="style2">B</div></td>
+  </tr>
+  <tr>
+    <td><span class="style1">Pengantar Teknologi Informasi </span></td>
+    <td><div align="center" class="style1">85</div></td>
+    <td><div align="center" class="style1">85</div></td>
+    <td><div align="center" class="style1">88</div></td>
+    <td><div align="center" class="style1">86,5</div></td>
+    <td bgcolor="#FFFF00"><div align="center" class="style2">A</div></td>
+  </tr>
+  <tr>
+    <td><span class="style1">Rekayasa Web </span></td>
+    <td><div align="center" class="style1">75</div></td>
+    <td><div align="center" class="style1">80</div></td>
+    <td><div align="center" class="style1">70</div></td>
+    <td><div align="center" class="style1">74,5</div></td>
+    <td bgcolor="#FFFF00"><div align="center" class="style2">C</div></td>
+  </tr>
+  <tr>
+    <td><span class="style1">Rekayasa Perangkat Lunak </span></td>
+    <td><div align="center" class="style1">60</div></td>
+    <td><div align="center" class="style1">65</div></td>
+    <td><div align="center" class="style1">60</div></td>
+    <td><div align="center" class="style1">62,0</div></td>
+    <td bgcolor="#FFFF00"><div align="center" class="style2">C</div></td>
+  </tr>
+  <tr>
+    <td bgcolor="#FF0000"><div align="right" class="style1"><strong>RATA-RATA</strong></div></td>
+    <td bgcolor="#FF0000"><div align="center"><span class="style1"><strong>78</strong></span></div></td>
+    <td bgcolor="#FF0000"><div align="center"><span class="style1"><strong>80</strong></span></div></td>
+    <td bgcolor="#FF0000"><div align="center"><span class="style1"><strong>74,6</strong></span></div></td>
+    <td bgcolor="#FF0000"><div align="center"><span class="style1"><strong>77,1</strong></span></div></td>
+    <td bgcolor="#FF0000"><span class="style1"></span></td>
+  </tr>
+</table>
+<p align="center">&copy; 2008 by &lt;nim&gt; &lt;nama&gt; </p>
+</body>
+</html>
